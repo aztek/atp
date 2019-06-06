60cb7cfcee8f7e66dde17d30c90b6bd956d31fdc
aztek <evgeny.kotelnikov@gmail.com>
2019-06-06 13:34:14 +0200

    Uniform style of module header documentation

--- src/ATP.hs
+++ src/ATP.hs
@@ -1,10 +1,10 @@
--- |
--- Module       : ATP
--- Description  : Bindings to automated theorem provers.
--- Copyright    : (c) Evgenii Kotelnikov, 2019
--- License      : GPL-3
--- Maintainer   : evgeny.kotelnikov@gmail.com
--- Stability    : experimental
---
+{-|
+Module       : ATP
+Description  : Bindings to automated theorem provers.
+Copyright    : (c) Evgenii Kotelnikov, 2019
+License      : GPL-3
+Maintainer   : evgeny.kotelnikov@gmail.com
+Stability    : experimental
+-}
 
 module ATP where
--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,11 @@
--- |
--- Module       : Main
--- Description  : Test suite for the atp library.
--- Copyright    : (c) Evgenii Kotelnikov, 2019
--- License      : GPL-3
--- Maintainer   : evgeny.kotelnikov@gmail.com
--- Stability    : experimental
---
+{-|
+Module       : Main
+Description  : Test suite for the atp library.
+Copyright    : (c) Evgenii Kotelnikov, 2019
+License      : GPL-3
+Maintainer   : evgeny.kotelnikov@gmail.com
+Stability    : experimental
+-}
 
 module Main where
 
