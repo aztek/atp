ffe96652ac821d924b4ca2d797621618d05a719c
aztek <evgeny.kotelnikov@gmail.com>
2020-04-30 23:12:40 +0200

    Add QC generators for alpha-(in)equivalent expressions

---
+++ test/QuickCheckSpec/Generators/AlphaEquivalent.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+{-|
+Module       : QuickCheckSpec.Generators.AlphaEquivalent
+Description  : QuickCheck generators of alpha-equivalent and alpha-inequivalent
+               first-order expressions.
+Copyright    : (c) Evgenii Kotelnikov, 2020
+License      : GPL-3
+Maintainer   : evgeny.kotelnikov@gmail.com
+Stability    : experimental
+-}
+
+module QuickCheckSpec.Generators.AlphaEquivalent (
+  genAlphaEquivalent,
+  genAlphaInequivalent
+) where
+
+import Control.Monad.Trans (lift)
+
+import Test.QuickCheck (Arbitrary(..), Gen, suchThat, elements)
+
+import ATP.FOL
+
+
+-- * Alpha-equivalent first-order expressions.
+
+genAlphaEquivalent :: FirstOrder e => e -> Gen e
+genAlphaEquivalent = getAlphaEquivalence . evalAlphaT . alpha
+
+newtype AlphaEquivalence m a = AlphaEquivalence { getAlphaEquivalence :: m a }
+  deriving (Functor, Applicative, Monad)
+
+instance AlphaMonad (AlphaEquivalence Gen) where
+  binding _  = fresh
+  occurrence = return
+
+
+-- * Alpha-inequivalent first-order expressions.
+
+genAlphaInequivalent :: FirstOrder e => e -> Gen e
+genAlphaInequivalent = getAlphaInequivalence . evalAlphaT . alpha
+
+newtype AlphaInequivalence m a = AlphaInequivalence { getAlphaInequivalence :: m a }
+  deriving (Functor, Applicative, Monad)
+
+instance AlphaMonad (AlphaInequivalence Gen) where
+  binding _  = stale
+  occurrence = anythingBut
+
+
+-- * Helper functions
+
+fresh :: AlphaT (AlphaEquivalence Gen) Var
+fresh = do
+  vs <- scope
+  lift . AlphaEquivalence $ arbitrary `suchThat` (`notElem` vs)
+
+stale :: AlphaT (AlphaInequivalence Gen) Var
+stale = do
+  vs <- scope
+  lift . AlphaInequivalence $ if null vs then arbitrary else elements vs
+
+anythingBut :: Var -> AlphaT (AlphaInequivalence Gen) Var
+anythingBut v = lift . AlphaInequivalence $ arbitrary `suchThat` (/= v)
