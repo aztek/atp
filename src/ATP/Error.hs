e53f75a134a1080329bae08460da1cfe802d8e54
aztek <evgeny.kotelnikov@gmail.com>
2020-11-03 22:19:14 +0100

    Add Error and Partial

---
+++ src/ATP/Error.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveTraversable #-}
+
+{-|
+Module       : ATP.Error
+Description  : Monads and monad transformers for computations with errors.
+Copyright    : (c) Evgenii Kotelnikov, 2019-2020
+License      : GPL-3
+Maintainer   : evgeny.kotelnikov@gmail.com
+Stability    : experimental
+-}
+
+module ATP.Error (
+  Error(..),
+  Partial,
+  PartialT(..),
+  liftPartial,
+  timeoutError,
+  parsingError,
+  proofError,
+  otherError
+) where
+
+import Control.Monad.Except (MonadTrans, ExceptT(..), MonadError(..), runExcept)
+import Data.Functor.Identity (Identity)
+import Data.Text (Text)
+import qualified Data.Text as T (pack)
+
+
+-- | The error that might occur while reconstructing the proof.
+data Error
+  = Timeout
+  -- ^ The theorem prover terminated by the timeout without producing a proof.
+  | ParsingError Text
+  -- ^ The output of the theorem prover is not a well-formed TSTP.
+  | ProofError Text
+  -- ^ The proof returned by the theorem prover is not well-formed.
+  | OtherError Text
+  -- ^ An uncategorized error.
+  deriving (Show, Eq, Ord)
+
+-- | The type of computations that might fail with an @'Error'@.
+type Partial = PartialT Identity
+
+-- | A monad transformer that adds failing with an @'Error'@ to other monads.
+newtype PartialT m a = PartialT {
+  runPartialT :: ExceptT Error m a
+} deriving (Show, Eq, Ord, Functor, Applicative, Monad, MonadTrans, MonadError Error)
+
+-- | Extractor for computations in the @'Partial'@ monad.
+liftPartial :: Partial a -> Either Error a
+liftPartial = runExcept . runPartialT
+
+-- | A smart constructor for a computation failed with a @'Timeout'@.
+timeoutError :: Monad m => PartialT m a
+timeoutError = PartialT (throwError Timeout)
+
+-- | A smart constructor for a computation failed with a @'ParsingError'@.
+parsingError :: Monad m => String -> PartialT m a
+parsingError = PartialT . throwError . ParsingError . T.pack
+
+-- | A smart constructor for a computation failed with a @'ProofError'@.
+proofError :: Monad m => String -> PartialT m a
+proofError = PartialT . throwError . ProofError . T.pack
+
+-- | A smart constructor for a computation failed with a @'OtherError'@.
+otherError :: Monad m => String -> PartialT m a
+otherError = PartialT . throwError . OtherError . T.pack
