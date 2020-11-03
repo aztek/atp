e39d2c218f82f4278847d25db8852277f1d95a51
aztek <evgeny.kotelnikov@gmail.com>
2020-11-03 22:18:56 +0100

    Add EnumerationT

--- src/ATP/Internal/Enumeration.hs
+++ src/ATP/Internal/Enumeration.hs
@@ -3,8 +3,8 @@
 
 {-|
 Module       : ATP.Internal.Enumeration
-Description  : The helper Enumeration monad used to describe computations with
-               renaming values with consecutive numbers.
+Description  : The helper Enumeration monad used to describe computations that
+               carry on a renaming of values to consecutive numbers.
 Copyright    : (c) Evgenii Kotelnikov, 2019-2020
 License      : GPL-3
 Maintainer   : evgeny.kotelnikov@gmail.com
@@ -12,6 +12,8 @@ Stability    : experimental
 -}
 
 module ATP.Internal.Enumeration (
+  EnumerationT(..),
+  evalEnumerationT,
   Enumeration,
   evalEnumeration,
   next,
@@ -19,24 +21,31 @@ module ATP.Internal.Enumeration (
   alias
 ) where
 
-import Control.Monad.State (MonadState, State, evalState, gets, modify)
+import Control.Monad.State (MonadTrans, MonadState, StateT, evalStateT, gets, modify)
+import Data.Functor.Identity (Identity(..))
 import Data.Map (Map)
 import qualified Data.Map as M (empty, lookup, insert)
 
 
-newtype Enumeration a s = Enumeration (State (Integer, Map a Integer) s)
-  deriving (Functor, Applicative, Monad, MonadState (Integer, Map a Integer))
+newtype EnumerationT a m s = EnumerationT {
+  runEnumerationT :: StateT (Integer, Map a Integer) m s
+} deriving (Functor, Applicative, Monad, MonadTrans, MonadState (Integer, Map a Integer))
+
+evalEnumerationT :: Monad m => EnumerationT a m e -> m e
+evalEnumerationT e = evalStateT (runEnumerationT e) (1, M.empty)
+
+type Enumeration a = EnumerationT a Identity
 
 evalEnumeration :: Enumeration a e -> e
-evalEnumeration (Enumeration s) = evalState s (1, M.empty)
+evalEnumeration = runIdentity . evalEnumerationT
 
-next :: Enumeration a Integer
+next :: Monad m => EnumerationT a m Integer
 next = do
   i <- gets fst
   modify $ \(j, m) -> (j + 1, m)
   return i
 
-enumerate :: Ord a => a -> Enumeration a Integer
+enumerate :: (Ord a, Monad m) => a -> EnumerationT a m Integer
 enumerate v = gets (M.lookup v . snd) >>= \case
   Just w -> return w
   Nothing -> do
@@ -44,7 +53,7 @@ enumerate v = gets (M.lookup v . snd) >>= \case
     modify $ fmap (M.insert v i)
     return i
 
-alias :: Ord a => a -> a -> Enumeration a ()
+alias :: (Ord a, Monad m) => a -> a -> EnumerationT a m ()
 alias a b = gets (\(_, m) -> (M.lookup a m, M.lookup b m)) >>= \case
   (Just{},  Just{})  -> error "alias"
   (Just i,  Nothing) -> modify $ fmap (M.insert b i)
