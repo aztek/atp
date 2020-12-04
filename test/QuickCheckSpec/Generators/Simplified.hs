be0d9e8e610ec94bb1b094e68183da4f71f6511b
aztek <evgeny.kotelnikov@gmail.com>
2020-12-04 15:13:56 +0100

    Remove unused instances

--- test/QuickCheckSpec/Generators/Simplified.hs
+++ test/QuickCheckSpec/Generators/Simplified.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 {-|
 Module       : QuickCheckSpec.Generators
@@ -24,26 +23,6 @@ import ATP.FOL
 newtype Simplified a = Simplified { getSimplified :: a }
   deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
 
-instance {-# OVERLAPPABLE #-} (Simplify e, Arbitrary e) => Arbitrary (Simplified e) where
+instance (Simplify e, Arbitrary e) => Arbitrary (Simplified e) where
   arbitrary = Simplified . simplify <$> arbitrary
   shrink = traverse (fmap simplify . shrink)
-
-instance Arbitrary f => Arbitrary (Simplified (Inference f)) where
-  arbitrary = do
-    i <- Inference <$> arbitrary <*> fmap getSimplified arbitrary
-    return (Simplified i)
-
-instance Arbitrary f => Arbitrary (Simplified (Sequent f)) where
-  arbitrary = do
-    s <- Sequent <$> arbitrary <*> fmap getSimplified arbitrary
-    return (Simplified s)
-
-instance (Ord f, Arbitrary f) => Arbitrary (Simplified (Derivation f)) where
-  arbitrary = do
-    d <- Derivation . fmap getSimplified <$> arbitrary
-    return (Simplified d)
-
-instance (Ord f, Arbitrary f) => Arbitrary (Simplified (Refutation f)) where
-  arbitrary = do
-    r <- Refutation <$> fmap getSimplified arbitrary <*> arbitrary
-    return (Simplified r)
